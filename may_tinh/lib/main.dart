58d1e89fe64ae9d6ab9c57d817437d3e57bdc0ad
Kietdeptraiquatroi <0306191239@caothang.edu.vn>
2021-10-30 22:18:08 +0700

    Giao Diện Và Chức Năng Hoàn Chỉnh

--- may_tinh/lib/main.dart
+++ may_tinh/lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:developer';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -31,45 +34,89 @@ class MyHomePage extends StatefulWidget {
 
 // Nguyễn Văn Su Ren
 class _MyHomePageState extends State<MyHomePage> {
-  int so1 = 0, so2 = 0;
-  String PhepTinh = "", KetQua = "", text = "0";
+  String so1 = "", so2 = "", KetQua = "", text = "";
+  String PhepTinh = "";
+  String lable1 = "", lable2 = "0";
+  int test = 0;
 
   void btnClicked(String btnText) {
-    //
-    if (btnText == "C") {
-      //
-      KetQua = ""; //
-      text = ""; //
-      so1 = 0; //
-      so2 = 0; //
+    if (btnText == "AC") {
+      test = 0;
+      text = "";
+      KetQua = "";
+      so1 = "";
+      so2 = "";
+      PhepTinh = "";
+      lable1 = "";
+      lable2 = "0";
+    } else if (btnText == "C") {
+      if (so2 == "") {
+        if (PhepTinh == "") {
+          lable1 = lable1.substring(0, lable1.length - 1);
+          KetQua = KetQua.substring(0, KetQua.length - 1);
+        } else {
+          if (lable1 != (PhepTinh + so1)) {
+            lable1 = lable1.substring(0, lable1.length - 1);
+            KetQua = KetQua.substring(0, KetQua.length - 1);
+          } else {
+            lable1 = lable1.substring(0, lable1.length - 1);
+            PhepTinh = "";
+          }
+        }
+      } else {
+        lable1 = lable1.substring(0, lable1.length - 1);
+        KetQua = KetQua.substring(0, KetQua.length - 1);
+      }
     } else if (btnText == "+" || //
         btnText == "-" || //
-        btnText == "x" || //
+        btnText == "x" ||
+        btnText == "%" || //
         btnText == "/") {
       //
-      so1 = int.parse(text); //
-      KetQua = "";
 
-      ///
-      PhepTinh = btnText; //
+      so1 = KetQua;
+      KetQua = "";
+      PhepTinh = btnText;
+      //
     } else if (btnText == "=") {
       //
-      so2 = int.parse(text); //
+      so2 = text; //
       if (PhepTinh == "+") {
-        //ưeuioaysujmdfhusauf
-        KetQua = (so1 + so2).toString();
-      } else if (PhepTinh == "-") {
-        KetQua = (so1 - so2).toString();
-      } else if (PhepTinh == "x") {
-        KetQua = (so1 * so2).toString();
-      } else if (PhepTinh == "/") {
-        KetQua = (so1 / so2).toString();
+        KetQua = (int.parse(so1) + int.parse(so2)).toString();
       }
+      if (PhepTinh == "-") {
+        KetQua = (int.parse(so1) - int.parse(so2)).toString();
+      }
+      if (PhepTinh == "x") {
+        KetQua = (int.parse(so1) * int.parse(so2)).toString();
+      }
+      if (PhepTinh == "/") {
+        KetQua = (int.parse(so1) / int.parse(so2)).toString();
+      }
+      if (PhepTinh == "%") {
+        KetQua = (int.parse(so1) % int.parse(so2)).toString();
+      }
+      test = 1;
+      lable2 = KetQua;
     } else {
-      KetQua = int.parse(text + btnText).toString();
+      if (test == 0) {
+        KetQua = KetQua + btnText;
+      } else {
+        test = 0;
+        KetQua = btnText;
+        so1 = "";
+        so2 = "";
+        text = "";
+        lable1 = "";
+      }
     }
+
     setState(() {
       text = KetQua;
+      if (btnText == "AC" || btnText == "C" || btnText == "=") {
+      } else {
+        lable1 = lable1 + btnText;
+      }
     });
   }
 
@@ -96,22 +143,35 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Máy Tính"),
+        title: Center(child: Text("Máy Tính")),
       ),
       body: Container(
         child: Column(
           children: <Widget>[
             Expanded(
               child: Container(
-                color: Colors.black,
-                alignment: Alignment.bottomRight,
-                padding: EdgeInsets.all(10),
-                child: Text(text,
-                    style: TextStyle(
-                        fontSize: 50,
-                        fontWeight: FontWeight.w100,
-                        color: Colors.white)),
-              ),
+                  color: Colors.black,
+                  child: Column(
+                    children: [
+                      Expanded(
+                        child: Container(
+                          alignment: Alignment.topLeft,
+                          padding: EdgeInsets.all(10),
+                          child: Text(lable1,
+                              style:
+                                  TextStyle(fontSize: 50, color: Colors.white)),
+                        ),
+                      ),
+                      Container(
+                        alignment: Alignment.bottomRight,
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          lable2,
+                          style: TextStyle(fontSize: 50, color: Colors.white),
+                        ),
+                      ),
+                    ],
+                  )),
             ),
             Container(
               color: Colors.black,
@@ -120,10 +180,18 @@ class _MyHomePageState extends State<MyHomePage> {
                 children: [
                   Row(
                     children: [
+                      _buildButton("AC", Colors.grey.shade400),
+                      _buildButton("C", Colors.grey.shade400),
+                      _buildButton("%", Colors.grey.shade400),
+                      _buildButton("/", Colors.yellow),
+                    ],
+                  ),
+                  Row(
+                    children: [
                       _buildButton("7", Colors.grey.shade700),
                       _buildButton("8", Colors.grey.shade700),
                       _buildButton("9", Colors.grey.shade700),
-                      _buildButton("/", Colors.yellow),
+                      _buildButton("x", Colors.yellow),
                     ],
                   ),
                   Row(
@@ -131,7 +199,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       _buildButton("4", Colors.grey.shade700),
                       _buildButton("5", Colors.grey.shade700),
                       _buildButton("6", Colors.grey.shade700),
-                      _buildButton("x", Colors.yellow),
+                      _buildButton("-", Colors.yellow),
                     ],
                   ),
                   Row(
@@ -139,15 +207,15 @@ class _MyHomePageState extends State<MyHomePage> {
                       _buildButton("1", Colors.grey.shade700),
                       _buildButton("2", Colors.grey.shade700),
                       _buildButton("3", Colors.grey.shade700),
-                      _buildButton("-", Colors.yellow),
+                      _buildButton("+", Colors.yellow),
                     ],
                   ),
                   Row(
                     children: [
-                      _buildButton("C", Colors.grey.shade700),
                       _buildButton("0", Colors.grey.shade700),
-                      _buildButton("=", Colors.grey.shade700),
-                      _buildButton("+", Colors.yellow),
+                      _buildButton("", Colors.grey.shade700),
+                      _buildButton(".", Colors.grey.shade700),
+                      _buildButton("=", Colors.yellow),
                     ],
                   ),
                 ],
