9d6ad274f45cc02bad1b84f7c3a8a5505430854a
UchihaShaSuRen <0306191265@caothang.edu.vn>
2021-11-07 11:33:49 +0700

    Giao diện đăng nhập thành công

--- widget_dangnhap/lib/main.dart
+++ widget_dangnhap/lib/main.dart
@@ -330,3 +330,265 @@ class SecondRoute extends StatelessWidget {
     );
   }
 }
+
+//Nguyễn Văn Su Ren
+class lsView {
+  String label;
+  String Soluong;
+  bool _Checked;
+  Icon icon;
+
+  lsView(this.label, this.icon, this._Checked, this.Soluong);
+}
+
+class ThirdRouter extends StatefulWidget {
+  @override
+  _ThirdRouterState createState() => _ThirdRouterState();
+  ThirdRouter({Key? key}) : super(key: key);
+}
+
+class _ThirdRouterState extends State<ThirdRouter> {
+  List<lsView> _lsMaiboxes = [
+    lsView(
+        "All inboxes",
+        Icon(
+          Icons.all_inbox,
+          color: Colors.pink.shade100,
+        ),
+        false,
+        '1'),
+    lsView(
+        "ICloud",
+        Icon(
+          Icons.cloud,
+          color: Colors.pink.shade100,
+        ),
+        false,
+        '5'),
+    lsView(
+        "Gmail",
+        Icon(
+          Icons.mail,
+          color: Colors.pink.shade100,
+        ),
+        false,
+        '1'),
+    lsView(
+        "Hotmail",
+        Icon(
+          Icons.mail_rounded,
+          color: Colors.pink.shade100,
+        ),
+        false,
+        '1'),
+    lsView(
+        "VIP",
+        Icon(
+          Icons.star,
+          color: Colors.pink.shade100,
+        ),
+        false,
+        '1'),
+  ];
+
+  List<lsView> _lsSpecial = [
+    lsView(
+        "Secure",
+        Icon(
+          Icons.all_inbox,
+          color: Colors.pink.shade100,
+        ),
+        false,
+        '1'),
+    lsView(
+        "Notication",
+        Icon(
+          Icons.cloud,
+          color: Colors.pink.shade100,
+        ),
+        false,
+        '5'),
+  ];
+  List<lsView> selectedListTitle = [];
+
+  Widget _ListView(
+      String label, String Soluong, bool _Checked, Icon icon, int index) {
+    return Center(
+      child: ListTile(
+        leading: _Checked
+            ? Icon(
+                Icons.check_circle,
+                color: Colors.pink.shade100,
+              )
+            : Icon(
+                Icons.check_circle_outline,
+                color: Colors.pink.shade100,
+              ),
+        title: Row(
+          children: [
+            icon,
+            SizedBox(
+              width: 20,
+            ),
+            Text(label),
+          ],
+        ),
+        trailing: Text(Soluong),
+        onTap: () {
+          setState(
+            () {
+              if (label == 'All inboxes' ||
+                  label == 'Gmail' ||
+                  label == 'Hotmail' ||
+                  label == 'ICloud' ||
+                  label == 'VIP') {
+                _lsMaiboxes[index]._Checked = !_lsMaiboxes[index]._Checked;
+                if (_lsMaiboxes[index]._Checked == true) {
+                  selectedListTitle.add(lsView(label, icon, _Checked, Soluong));
+                } else if (_lsMaiboxes[index]._Checked == false) {
+                  selectedListTitle.removeWhere(
+                      (element) => element.label == _lsMaiboxes[index].label);
+                }
+              } else if (label == 'Secure' || label == 'Notication') {
+                _lsSpecial[index]._Checked = !_lsSpecial[index]._Checked;
+
+                if (_lsSpecial[index]._Checked == true) {
+                  selectedListTitle.add(lsView(label, icon, _Checked, Soluong));
+                } else if (_lsSpecial[index]._Checked == false) {
+                  selectedListTitle.removeWhere(
+                      (element) => element.label == _lsSpecial[index].label);
+                }
+              }
+            },
+          );
+        },
+      ),
+    );
+  }
+
+  Widget buildButton(BuildContext context, int index) {
+    return Container(
+      alignment: Alignment.bottomCenter,
+      padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
+      child: Center(
+        child: TextButton(
+          child: Text(
+            'DELETE (${selectedListTitle.length})',
+            style: TextStyle(
+              fontSize: 22,
+              color: Colors.white,
+            ),
+          ),
+          style: TextButton.styleFrom(
+              backgroundColor: Colors.pink.shade100,
+              minimumSize: Size(400, 55)),
+          onPressed: () => deleteItem(index),
+        ),
+      ),
+    );
+  }
+
+  void deleteItem(int index) {
+    for (int i = 0; i < selectedListTitle.length; i++) {
+      if (_lsMaiboxes[index]._Checked == true) {
+        selectedListTitle.removeWhere(
+            (element) => element.label == _lsMaiboxes[index].label);
+        _lsMaiboxes.removeAt(index);
+      }
+      if (_lsSpecial[index]._Checked == true) {
+        selectedListTitle
+            .removeWhere((element) => element.label == _lsSpecial[index].label);
+
+        _lsSpecial.removeAt(index);
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.pink.shade50,
+        title: Text(
+          "Mailboxes",
+          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        actions: <Widget>[
+          TextButton(
+              child: Text(
+                "Done",
+                style: TextStyle(
+                    color: Colors.pink.shade100,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold),
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => FirstRoute()),
+                );
+              },
+              style: ButtonStyle(
+                foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+              ))
+        ],
+      ),
+      body: SafeArea(
+        child: Container(
+          child: Column(
+            children: [
+              ListTile(
+                tileColor: Colors.pink.shade100,
+                title: Text('Mailboxes'),
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: _lsMaiboxes.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return _ListView(
+                      _lsMaiboxes[index].label,
+                      _lsMaiboxes[index].Soluong,
+                      _lsMaiboxes[index]._Checked,
+                      _lsMaiboxes[index].icon,
+                      index,
+                    );
+                  },
+                ),
+              ),
+              ListTile(
+                tileColor: Colors.pink.shade100,
+                title: Text('Special folders'),
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: _lsSpecial.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return _ListView(
+                      _lsSpecial[index].label,
+                      _lsSpecial[index].Soluong,
+                      _lsSpecial[index]._Checked,
+                      _lsSpecial[index].icon,
+                      index,
+                    );
+                  },
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  alignment: Alignment.bottomCenter,
+                  child: ListView.builder(
+                    itemCount: 1,
+                    itemBuilder: (BuildContext context, int index) {
+                      return buildButton(context, index);
+                    },
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
